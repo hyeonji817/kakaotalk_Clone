98009dce96a01b726654cf48babbf90bece16b44
hyeonji817 <guswl930817@gmail.com>
2025-08-31 22:28:46 +0900

    footer 페이지와 index.css 페이지 코드 일부 수정

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
       <ul>
         <li>
           <a href="/"><span className="ft_f_list">친구목록</span></a>
-          <a href="/chat.html"><span className="ft_c_list">채팅목록</span></a>
+          <a href="/chats"><span className="ft_c_list">채팅목록</span></a>
           <a href="#"><span className="ft_page">페이지</span></a>
           <a href="#"><span className="ft_more">더보기</span></a>
         </li>
